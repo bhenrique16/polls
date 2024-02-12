be32969b314dd45411ac2e867608a427aecb96c4
bhenrique16 <bh.henrique16@gmail.com>
2024-02-12 20:41:43 -0300

    Initialized project and installed main dependencies

---
+++ src/http/server.ts
@@ -0,0 +1,25 @@
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+
+const app = fastify();
+const prisma = new PrismaClient();
+
+app.post("/polls", async (request, reply) => {
+  const createPoolBody = z.object({
+    title: z.string(),
+  });
+  const { title } = createPoolBody.parse(request.body);
+
+  const poll = await prisma.polls.create({
+    data: {
+      title,
+    },
+  });
+
+  return reply.status(201).send({ pollID: poll.id });
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("http server running");
+});
